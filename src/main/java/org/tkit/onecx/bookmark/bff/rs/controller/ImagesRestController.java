3348499b4be9b2b3171ee29d7cc9c5fc113e54d8
JordenReuter <149687553+JordenReuter@users.noreply.github.com>
2025-05-24 06:24:19 +0200

    feat: added images and aligned to svc (#54)

--- src/main/java/org/tkit/onecx/bookmark/bff/rs/controller/ImagesRestController.java
+++ src/main/java/org/tkit/onecx/bookmark/bff/rs/controller/ImagesRestController.java
@@ -1,5 +1,7 @@
 package org.tkit.onecx.bookmark.bff.rs.controller;
 
+import java.io.File;
+
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
@@ -7,9 +9,16 @@ import jakarta.ws.rs.core.HttpHeaders;
 import jakarta.ws.rs.core.Response;
 
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jboss.resteasy.reactive.ClientWebApplicationException;
+import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
+import org.tkit.onecx.bookmark.bff.rs.mappers.BookmarkMapper;
+import org.tkit.onecx.bookmark.bff.rs.mappers.ExceptionMapper;
 import org.tkit.quarkus.log.cdi.LogService;
 
 import gen.org.tkit.onecx.bookmark.bff.rs.internal.ImagesInternalApiService;
+import gen.org.tkit.onecx.bookmark.bff.rs.internal.model.ImageInfoDTO;
+import gen.org.tkit.onecx.bookmark.image.client.api.ImagesInternalApi;
+import gen.org.tkit.onecx.bookmark.image.client.model.ImageInfo;
 import gen.org.tkit.onecx.product.store.client.api.ImagesApi;
 
 @ApplicationScoped
@@ -20,6 +29,46 @@ public class ImagesRestController implements ImagesInternalApiService {
     @Inject
     @RestClient
     ImagesApi productStoreImageClient;
+
+    @Inject
+    @RestClient
+    ImagesInternalApi bookmarkImageClient;
+
+    @Inject
+    HttpHeaders headers;
+
+    @Inject
+    BookmarkMapper mapper;
+
+    @Inject
+    ExceptionMapper exceptionMapper;
+
+    @Override
+    public Response deleteImage(String refId) {
+        try (Response response = bookmarkImageClient.deleteImage(refId)) {
+            return Response.status(response.getStatus()).build();
+        }
+    }
+
+    @Override
+    public Response getImage(String refId) {
+        Response.ResponseBuilder responseBuilder;
+        try (Response response = bookmarkImageClient.getImage(refId)) {
+            var contentType = response.getHeaderString(HttpHeaders.CONTENT_TYPE);
+            var contentLength = response.getHeaderString(HttpHeaders.CONTENT_LENGTH);
+            var body = response.readEntity(byte[].class);
+            if (contentType != null && body.length != 0) {
+                responseBuilder = Response.status(response.getStatus())
+                        .header(HttpHeaders.CONTENT_TYPE, contentType)
+                        .header(HttpHeaders.CONTENT_LENGTH, contentLength)
+                        .entity(body);
+            } else {
+                responseBuilder = Response.status(Response.Status.BAD_REQUEST);
+            }
+
+            return responseBuilder.build();
+        }
+    }
 
     @Override
     public Response getProductLogo(String productName) {
@@ -39,5 +88,18 @@ public class ImagesRestController implements ImagesInternalApiService {
 
             return responseBuilder.build();
         }
+    }
+
+    @Override
+    public Response uploadImage(String refId, File body) {
+        try (Response response = bookmarkImageClient.uploadImage(headers.getLength(), refId, body)) {
+            ImageInfoDTO imageInfoDTO = mapper.mapImageInfo(response.readEntity(ImageInfo.class));
+            return Response.status(response.getStatus()).entity(imageInfoDTO).build();
+        }
+    }
+
+    @ServerExceptionMapper
+    public Response restException(ClientWebApplicationException ex) {
+        return exceptionMapper.clientException(ex);
     }
 }
